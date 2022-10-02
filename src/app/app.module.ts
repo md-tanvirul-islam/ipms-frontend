b413c8b577bcd107d92226298affff5115890038
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-10-02 13:28:48 +0600

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +30,7 @@ import { IndexComponent as AuditIndexComponent } from './components/audit/index/
     EditComponent,
     AuditIndexComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public form = {
+    email: null,
+    password: null,
+  };
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.form);
+    return this.http.post('url', this.form).subscribe(
+      (data) => console.log(data),
+      (error) => console.log(error)
+    );
   }
-
 }
