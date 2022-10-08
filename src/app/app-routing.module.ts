eb2d991870c1a16a6ffc36cd436a39be61ee559c
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-10-08 19:05:09 +0600

    remove password-reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { CreateComponent } from './components/address/create/create.component';
 import { EditComponent } from './components/address/edit/edit.component';
 import { IndexComponent } from './components/address/index/index.component';
 import { LoginComponent } from './components/login/login.component';
-import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
-import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { IndexComponent as AuditIndexComponent } from './components/audit/index/index.component';
@@ -27,16 +25,6 @@ const routes: Routes = [
     path: 'profile',
     component: ProfileComponent,
     canActivate: [AfterLoginService]
-  },
-  {
-    path: 'request-password-reset',
-    component: RequestResetComponent,
-    canActivate: [BeforeLoginService]
-  },
-  {
-    path: 'response-password-reset',
-    component: ResponseResetComponent,
-    canActivate: [BeforeLoginService]
   },
   {
     path: 'address/index',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
-import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 import { IndexComponent } from './components/address/index/index.component';
 import { CreateComponent } from './components/address/create/create.component';
 import { EditComponent } from './components/address/edit/edit.component';
@@ -26,8 +24,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     LoginComponent,
     RegistrationComponent,
     ProfileComponent,
-    RequestResetComponent,
-    ResponseResetComponent,
     IndexComponent,
     CreateComponent,
     EditComponent,
